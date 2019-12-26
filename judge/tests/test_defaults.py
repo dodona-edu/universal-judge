2bc5a7a50dc47f13c34ea2563a2480ef153b4142
Niko Strijbol <strijbol.niko@gmail.com>
2019-12-26 17:06:11 +0100

    Fix a test

--- judge/tests/test_defaults.py
+++ judge/tests/test_defaults.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 from testplan import parse_test_plan
 
-FOLDER = Path(__file__).parent
+FOLDER = Path(__file__).parent / "testplan"
 FULL = FOLDER / "full.json"
 SHORT = FOLDER / "short.json"
 
 
 def test_correct_defaults():
+    """Tests that the testplan uses the correct defaults if values are omitted."""
     with open(FULL, 'r') as f:
         full = parse_test_plan(f.read())
 
