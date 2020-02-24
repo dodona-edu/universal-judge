6c0f870f2f77f8c0d30245b8826b05a3ca8cb0a1
Niko Strijbol <strijbol.niko@gmail.com>
2020-02-24 20:53:51 +0100

    Update ISBN exercise

--- exercise/isbn/preparation/generator.py
+++ exercise/isbn/preparation/generator.py
@@ -1,14 +1,58 @@
-import os
-import imp
-import sys
+"""
+Generate a testplan for the exercise.
+"""
 import random
 import string
+import json
 
 # set fixed seed
 random.seed(123456789)
 
-# helper function for computing the ISBN-10 check digit
-def checkdigit10(code):
+# Find the name of the testplan.
+with open("../config.json", "r") as config_file:
+    config = json.load(config_file)
+
+testplan_name = config.get("plan_name", "plan.json")
+
+
+def _encode(value):
+    """TODO: this is copied from the judge, provide a library?"""
+    if value is None:
+        type_ = "nothing"
+        data_ = value
+    elif isinstance(value, str):
+        type_ = "text"
+        data_ = value
+    elif isinstance(value, bool):
+        type_ = "boolean"
+        data_ = value
+    elif isinstance(value, int):
+        type_ = "integer"
+        data_ = value
+    elif isinstance(value, float):
+        type_ = "rational"
+        data_ = value
+
+    elif isinstance(value, list) or isinstance(value, tuple):
+        type_ = "list"
+        data_ = [_encode(x) for x in value]
+    elif isinstance(value, set):
+        type_ = "set"
+        data_ = [_encode(x) for x in value]
+    elif isinstance(value, dict):
+        type_ = "object"
+        data_ = {str(k): _encode(v) for k, v in value.items()}
+    else:
+        type_ = "unknown"
+        data_ = str(value)
+
+    return {
+        "data": data_,
+        "type": type_
+    }
+
+def check_digit10(code):
+    """helper function for computing the ISBN-10 check digit"""
     
     # compute the check digit
     check = sum((i + 1) * int(code[i]) for i in range(9)) % 11
@@ -16,8 +60,9 @@ def checkdigit10(code):
     # convert the check digit into string representation
     return 'X' if check == 10 else str(check)
 
-# helper function for computing the ISBN-10 check digit
-def checkdigit13(code):
+
+def check_digit13(code):
+    """helper function for computing the ISBN-13 check digit"""
     
     # compute the check digit
     check = sum((3 if i % 2 else 1) * int(code[i]) for i in range(12))
@@ -25,18 +70,25 @@ def checkdigit13(code):
     # convert the check digit into a single digit
     return str((10 - check) % 10)
 
-def randomCharacters(length, alphabet):
-    
+
+def random_characters(length, alphabet):
     return ''.join(random.choice(alphabet) for _ in range(length))
 
+
 # load functionality defined in sample solution
-solution = imp.load_source(
-    'solution', 
-    os.path.join('..', 'solution', 'solution.en.py')
-)
-for name in dir(solution):
-    if not (name.startswith('__') and name.endswith('__')):
-        globals()[name] = eval('solution.{}'.format(name))
+try:
+    from ..solution import solution
+except:
+    import solution
+
+contexts = []
+is_isbn = {
+    "name": "isISBN",
+    "contexts": contexts
+}
+plan = {
+    "tabs": [is_isbn]
+}
 
 # generate test cases for function isISBN
 args = [
@@ -48,30 +100,52 @@ args = [
     (9789027439642, None),
 ]
 while len(args) < 50:
-    
     length = random.choice([10, 13])
-    code = randomCharacters(length - 1, string.digits)
+    code = random_characters(length - 1, string.digits)
     if length == 10:
         if random.random() < 0.5:
-            code += checkdigit10(code)
+            code += check_digit10(code)
         else:
             code += random.choice(string.digits + 'X')
     else:
         if random.random() < 0.5:
-            code += checkdigit13(code)
+            code += check_digit13(code)
         else:
             code += random.choice(string.digits)
-    args.append((code, random.choice([None, True, False])))    
+    args.append((code, random.choice([None, True, False])))
 
 # generate unit tests for function isISBN
-sys.stdout = open(os.path.join('..', 'evaluation', '0.in'), 'w', encoding='utf-8')
 for code, isbn13 in args:
-    if isbn13 is None:
-        print(f'>>> isISBN({code!r})')
-    else:
-        print(f'>>> isISBN({code!r}, {isbn13})')
-    print(isISBN(code, isbn13 if isbn13 is not None else True))
-    print()
+    function_arguments = [
+        _encode(code)
+    ]
+    if isbn13 is not None:
+        function_arguments.append(_encode(isbn13))
+
+    isbn13_param = isbn13 if isbn13 is not None else True
+
+    output_ = {
+        "result": {
+            "value": (_encode(solution.isISBN(code, isbn13_param)))
+        }
+    }
+
+    testcase = {
+        "input":  {
+            "function": {
+                "type":      "top",
+                "name":      "isISBN",
+                "arguments": function_arguments
+            }
+        },
+        "output": output_
+    }
+
+    context = {
+        "normal": [testcase]
+    }
+
+    contexts.append(context)
 
 # generate test cases for function areISBN
 codes = ['0012345678', '0012345679', '9971502100', '080442957X', 5, True, 'The Practice of Computing Using Python', '9789027439642', '5486948320146']
@@ -91,15 +165,15 @@ while len(args) < 50:
     for _ in range(random.randint(4, 10)):
      
         length = random.choice([10, 13])
-        code = randomCharacters(length - 1, string.digits)
+        code = random_characters(length - 1, string.digits)
         if length == 10:
             if random.random() < 0.5:
-                code += checkdigit10(code)
+                code += check_digit10(code)
             else:
                 code += random.choice(string.digits + 'X')
         else:
             if random.random() < 0.5:
-                code += checkdigit13(code)
+                code += check_digit13(code)
             else:
                 code += random.choice(string.digits)
         codes.append(code)
@@ -107,13 +181,63 @@ while len(args) < 50:
     args.append((codes, random.choice([None, True, False])))    
 
 # generate unit tests for function areISBN
-sys.stdout = open(os.path.join('..', 'evaluation', '1.in'), 'w', encoding='utf-8')
+contexts = []
+are_isbn = {
+    "name": "areISBN",
+    "contexts": contexts
+}
+plan["tabs"].append(are_isbn)
 for index, (codes, isbn13) in enumerate(args):
     index += 1
-    print(f'>>> codes{index:02d} = {codes} # doctest: +NEWCONTEXT')
-    if isbn13 is None:
-        print(f'>>> areISBN(codes{index:02d})')
-    else:
-        print(f'>>> areISBN(codes{index:02d}, {isbn13})')
-    print(areISBN(codes, isbn13))
-    print()
+    assignment_testcase = {
+        "input": {
+            "assignment": {
+                "name": f"codes{index:02d}",
+                "expression": {
+                    "type": "identity",
+                    "arguments": [
+                        _encode(codes)
+                    ]
+                }
+            }
+        }
+    }
+
+    function_arguments = [
+        {
+            "type": "literal",
+            "data": f"codes{index:02d}"
+        }
+    ]
+
+    if isbn13 is not None:
+        function_arguments.append(_encode(isbn13))
+
+    isbn13_param = isbn13 if isbn13 is not None else True
+
+    output_ = {
+        "result": {
+            "value": (_encode(solution.areISBN(codes, isbn13_param)))
+        }
+    }
+
+    testcase = {
+        "input":  {
+            "function": {
+                "type":      "top",
+                "name":      "areISBN",
+                "arguments": function_arguments
+            }
+        },
+        "output": output_
+    }
+
+    context = {
+        "normal": [assignment_testcase, testcase]
+    }
+
+    contexts.append(context)
+
+
+with open(testplan_name, 'w') as fp:
+    json.dump(plan, fp, indent=2)
