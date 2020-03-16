b4a26f12ac999e9dbe2d9d89da234f2ad806f088
Niko Strijbol <strijbol.niko@gmail.com>
2020-03-16 18:07:11 +0100

    More examples

--- judge/runners/templates/java/AbstractCustomEvaluator.java
+++ judge/runners/templates/java/AbstractCustomEvaluator.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.util.List;
 
 abstract class AbstractCustomEvaluator extends AbstractEvaluator {
-
     abstract void evaluate(Object expected,
                            Object actual,
                            List<Object> arguments) throws IOException;
--- judge/runners/templates/java/AbstractSpecificEvaluator.java
+++ judge/runners/templates/java/AbstractSpecificEvaluator.java
@@ -2,6 +2,5 @@ import java.io.IOException;
 import java.util.List;
 
 abstract class AbstractSpecificEvaluator extends AbstractEvaluator {
-
     abstract void evaluate(Object actual) throws IOException;
 }
