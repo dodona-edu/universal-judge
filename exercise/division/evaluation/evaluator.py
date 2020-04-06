bca847adecc67b9a40897d340e6492ad2a19b4d1
Niko Strijbol <strijbol.niko@gmail.com>
2020-04-06 22:50:55 +0200

    Tweak evaluator

--- exercise/division/evaluation/evaluator.py
+++ exercise/division/evaluation/evaluator.py
@@ -4,11 +4,14 @@ import specific_evaluation_utils as u
 
 def evaluate(value):
     if isinstance(value, ZeroDivisionError):
+        # If a zero division error, show the stacktrace.
         formatted = "".join(traceback.format_exception(type(value), value, value.__traceback__))
         u.evaluated(True, formatted, formatted)
     elif isinstance(value, Exception):
+        # If another error, show the stacktrace as well.
         formatted = "".join(traceback.format_exception(type(value), value, value.__traceback__))
-        u.evaluated(False, "ZeroDivisionError", formatted, ["Verwachtte een ZeroDivisionError."])
+        u.evaluated(False, "ZeroDivisionError", formatted, [f"Verwachtte een ZeroDivisionError, maar kreeg een {type(value).__name__}."])
     else:
+        # Else show the str of the value.
         actual = str(value) if value else ""
         u.evaluated(False, "ZeroDivisionError", actual, ["Verwachtte een ZeroDivisionError."])
