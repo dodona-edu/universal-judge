456100b17c82173496b9bf8c45dc5160a9ecb21c
Niko Strijbol <strijbol.niko@gmail.com>
2020-04-30 18:42:51 +0200

    Put functions in same file

--- exercise/echo/evaluation/evaluator-correct.py
+++
@@ -1,5 +0,0 @@
-from evaluation_utils import EvaluationResult
-
-
-def evaluate(expected, actual, arguments):
-    return EvaluationResult(expected.strip() == actual.strip())
--- exercise/echo/evaluation/evaluator-wrong.py
+++
@@ -1,5 +0,0 @@
-from evaluation_utils import EvaluationResult
-
-
-def evaluate(expected, actual, arguments):
-    return EvaluationResult(False)
