38eca463e25711ec6259795097aaac6f810cc2cc
Niko Strijbol <strijbol.niko@gmail.com>
2020-05-10 19:25:03 +0200

    Pass memory to JVM

--- judge/src/tested/languages/java/config.py
+++ judge/src/tested/languages/java/config.py
@@ -4,6 +4,15 @@ from typing import List
 from tested.languages import Language
 from tested.languages.config import CallbackResult, Command, Config
 
+def _memory_limit(config: Config) -> int:
+    """
+    Get the memory limit in bytes. Java requires this to be a multiple of 1024.
+    See https://docs.oracle.com/en/java/javase/14/docs/specs/man/java.html
+    """
+    limit = int(config.memory_limit)
+    limit = (limit // 1024) * 1024
+    return limit
+
 
 class Java(Language):
 
@@ -15,4 +24,5 @@ class Java(Language):
 
     def execution(self, config: Config, 
                   cwd: Path, file: str, arguments: List[str]) -> Command:
-        return ["java", "-cp", ".", Path(file).stem, *arguments]
+        limit = _memory_limit(config)
+        return ["java", f"-Xmx{limit}", "-cp", ".", Path(file).stem, *arguments]
