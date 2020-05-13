6b28e6201444fb192e5238473d43e344f4c17150
Niko Strijbol <strijbol.niko@gmail.com>
2020-05-13 17:52:08 +0200

    Execute javascript file with node

--- judge/src/tested/languages/javascript/config.py
+++ judge/src/tested/languages/javascript/config.py
@@ -2,12 +2,11 @@ from pathlib import Path
 from typing import List
 
 from tested.languages import Language
-from tested.languages.config import executable_name, Command, Config
+from tested.languages.config import Command, Config
 
 
 class JavaScript(Language):
 
     def execution(self, config: Config,
                   cwd: Path, file: str, arguments: List[str]) -> Command:
-        local_file = cwd / executable_name(Path(file).stem)
-        return ['node', str(local_file.absolute()), *arguments]
+        return ['node', file, *arguments]
