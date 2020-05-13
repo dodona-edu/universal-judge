cd7e7823e6f4223142538b083c2d379659cc01f8
Niko Strijbol <strijbol.niko@gmail.com>
2020-05-13 16:34:21 +0200

    Update parameters

--- judge/src/tested/languages/javascript/config.py
+++ judge/src/tested/languages/javascript/config.py
@@ -2,11 +2,12 @@ from pathlib import Path
 from typing import List
 
 from tested.languages import Language
-from tested.languages.config import executable_name, Command
+from tested.languages.config import executable_name, Command, Config
 
 
 class JavaScript(Language):
 
-    def execution(self, cwd: Path, file: str, arguments: List[str]) -> Command:
+    def execution(self, config: Config,
+                  cwd: Path, file: str, arguments: List[str]) -> Command:
         local_file = cwd / executable_name(Path(file).stem)
         return ['node', str(local_file.absolute()), *arguments]
