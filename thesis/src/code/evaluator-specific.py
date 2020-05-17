23a7e5c3769b62e1f28fefece293a82b7a34385c
Niko Strijbol <strijbol.niko@gmail.com>
2020-05-17 22:39:53 +0200

    Apply suggestions part I/III

--- thesis/src/code/evaluator-specific.py
+++ thesis/src/code/evaluator-specific.py
@@ -1,8 +1,8 @@
-from evaluation_utils import EvaluationResult
+from evaluation_utils import EvaluationResult, Message
 
 def evaluate(actual):
     correct = isinstance(actual, int)
     messages = []
     if not correct:
-        messages.append("Dit is geen int!")
+        messages.append(Message("Dit is geen int!"))
     return EvaluationResult(correct, str(5), str(actual), messages)
