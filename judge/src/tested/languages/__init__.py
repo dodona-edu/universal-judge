963a8b791f82b9be05f136775f8022fea242dffe
Boris Sels <boris.sels@ugent.be>
2020-09-24 18:07:41 +0200

    Kotlin python config

--- judge/src/tested/languages/__init__.py
+++ judge/src/tested/languages/__init__.py
@@ -6,20 +6,21 @@ needed to execute the templates.
 In short, if it has to do with the templates or is programming language specific,
 you will probably find it in this package.
 """
-from .config import Language
 from tested.languages.c.config import C
 from tested.languages.haskell.config import Haskell
 from tested.languages.java.config import Java
 from tested.languages.javascript.config import JavaScript
+from tested.languages.kotlin.config import Kotlin
 from tested.languages.python.config import Python
 from tested.languages.runhaskell.config import RunHaskell
-
+from .config import Language
 
 LANGUAGES = {
     'c':          C,
     'haskell':    Haskell,
     'java':       Java,
     'javascript': JavaScript,
+    'kotlin':     Kotlin,
     'python':     Python,
     'runhaskell': RunHaskell,
 }
--- judge/src/tested/languages/kotlin/config.py
+++ judge/src/tested/languages/kotlin/config.py
@@ -4,11 +4,23 @@ from typing import List
 from tested.languages import Language
 from tested.languages.config import CallbackResult, Command, Config
 
+def _memory_limit(config: Config) -> int:
+    """
+    Get the memory limit in bytes. Java requires this to be a multiple of 1024.
+    See https://docs.oracle.com/en/java/javase/14/docs/specs/man/java.html
+    """
+    limit = int(config.memory_limit)
+    limit = (limit // 1024) * 1024
+    return limit
 
 class Kotlin(Language):
 
     def compilation(self, config: Config, files: List[str]) -> CallbackResult:
-        raise NotImplementedError
+        def file_filter(file: Path) -> bool:
+            return file.suffix == ".class"
+        others = [x for x in files if not x.endswith(".jar")]
+        return ["kotlinc", "-jvm-target", "11", "-cp", ".", *others], file_filter
 
     def execution(self, config: Config, cwd: Path, file: str, arguments: List[str]) -> Command:
-        raise NotImplementedError
+        limit = _memory_limit(config)
+        return ["kotlin", f"-J-Xmx{limit}", "-cp", ".", Path(file).stem, *arguments]
