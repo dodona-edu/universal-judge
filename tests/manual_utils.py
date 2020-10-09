8dd66e8e07cb00b05588d0a144a8cecd90f3f379
Niko Strijbol <strijbol.niko@gmail.com>
2020-10-09 14:25:06 +0200

    Change directory structure

--- tests/manual_utils.py
+++ tests/manual_utils.py
@@ -1,7 +1,5 @@
 import json
-import os
 import re
-import shutil
 import threading
 from io import StringIO
 from pathlib import Path
@@ -71,7 +69,7 @@ def configuration(config, exercise: str, language: str, work_dir: Path,
     ext = get_language(language).extension_file()
     if options is None:
         options = {}
-    exercise_dir = Path(config.rootdir).parent / "exercise"
+    exercise_dir = Path(config.rootdir) / "exercise"
     ep = f'{exercise_dir}/{exercise}'
     return DodonaConfig(**merge({
         "memory_limit":         536870912,
@@ -80,7 +78,7 @@ def configuration(config, exercise: str, language: str, work_dir: Path,
         "natural_language":     'nl',
         "resources":            Path(f'{ep}/evaluation'),
         "source":               Path(f'{ep}/solution/{solution}.{ext}'),
-        "judge":                Path(f'{config.rootdir}/src'),
+        "judge":                Path(f'{config.rootdir}'),
         "workdir":              work_dir,
         "plan_name":            plan,
     }, options))
