126960c57352829def27a11be31b524f513dad3e
Boris Sels <boris.sels@ugent.be>
2020-12-04 11:30:30 +0100

    Allow positional arguments

--- tested/translate_dsl.py
+++ tested/translate_dsl.py
@@ -4,14 +4,29 @@ from tested.utils import smart_close
 
 if __name__ == "__main__":
     parser = ArgumentParser(
-        description="The universal judge for Dodona."
+        description="The universal judge for Dodona.",
+        usage='%(prog)s [-h] [-i DSL] [-o JSON] [dsl [json]]'
     )
     parser.add_argument('-i', '--dsl', type=FileType('r'),
-                        help="Where the test plan should be read from.", default="-")
+                        help="Where the test plan should be read from.",
+                        default="-")
     parser.add_argument('-o', '--json', type=FileType('w'),
                         help="Where the translate test plan should be written to.",
                         default="-")
+    parser.add_argument('dsl_file', metavar='dsl', nargs='?', type=FileType('r'),
+                        help="Where the test plan should be read from, override "
+                             "option when given.")
+    parser.add_argument('json_file', metavar='json', nargs='?', type=FileType('w'),
+                        help="Where the translate test plan should be written to, "
+                             "override option when given.")
+
     parser = parser.parse_args()
+    if parser.dsl_file is not None:
+        smart_close(parser.dsl)
+        parser.dsl = parser.dsl_file
+        if parser.json_file is not None:
+            smart_close(parser.json)
+            parser.json = parser.json_file
 
     with smart_close(parser.dsl) as test_plan:
         yaml_str = test_plan.read()
