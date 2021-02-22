eda0047fdf601f38b312e6c017c8c0a11a37a07d
Boris Sels <boris.sels@gmail.com>
2021-02-22 16:24:18 +0100

    Add benchmarks combo_breaker

---
+++ benchmarking/__init__.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+
+from tested.configs import DodonaConfig
+from tested.main import run
+
+tmp_dir = Path("generated")
+
+
+@dataclass
+class BenchmarkExercise:
+    name: str
+    plan: str
+    solution: str
+
+
+@dataclass
+class BenchmarkResult:
+    exercise: BenchmarkExercise
+    min_time: float
+    avg_time: float
+    max_time: float
+
+
+@dataclass
+class BenchmarkResults:
+    results: List[BenchmarkResult]
+
+
+all_exercises = [
+    BenchmarkExercise(
+        name="combo_breaker",
+        plan="plan_opt.json",
+        solution="solution.py"
+    ),
+    BenchmarkExercise(
+        name="combo_breaker",
+        plan="plan_opt.json",
+        solution="solution.old.py"
+    ),
+    BenchmarkExercise(
+        name="combo_breaker",
+        plan="plan_no_opt.json",
+        solution="solution.py"
+    ),
+    BenchmarkExercise(
+        name="combo_breaker",
+        plan="plan_no_opt.json",
+        solution="solution.old.py"
+    )
+]
+
+
+def get_config(exercise: BenchmarkExercise,
+               index: int,
+               parallel: bool = True, ) -> DodonaConfig:
+    exercise_dir = Path(__file__).parent / 'tests' / exercise.name
+    exercise_tmp_dir = get_temp_dir(
+        f"{exercise.name}_{exercise.solution.replace('.', '_')}_"
+        f"{exercise.plan.replace('.', '_')}_{index}"
+    )
+    exercise_tmp_dir.mkdir(parents=True)
+    return DodonaConfig(**{
+        "memory_limit":         536870912,  # 500 MB
+        "time_limit":           120,  # Two minutes
+        "programming_language": "python",
+        "natural_language":     'nl',
+        "resources":            exercise_dir / 'evaluation',
+        "source":               exercise_dir / 'solution' / exercise.solution,
+        "judge":                Path('.'),
+        "workdir":              exercise_tmp_dir,
+        "plan_name":            exercise.plan,
+        "options":              {
+            "parallel": parallel,
+            "mode":     "batch",
+            "linter":   {
+                "python": False
+            }
+        }
+    })
+
+
+def get_temp_dir(exercise_name: str) -> Path:
+    return Path(tmp_dir, exercise_name)
+
+
+def time_exercise(exercise: BenchmarkExercise,
+                  times: int = 10) -> BenchmarkResult:
+    def timing() -> float:
+        config = get_config(exercise, i)
+        start = time.perf_counter()
+        run(config, open(os.devnull, "w"))
+        # run(config, sys.stdout)
+        end = time.perf_counter()
+        return end - start
+
+    timing_results = []
+
+    for i in range(1, times + 1):
+        print(f"Time {i}/{times}")
+        timing_results.append(timing())
+
+    return BenchmarkResult(
+        exercise=exercise,
+        min_time=min(timing_results),
+        avg_time=sum(timing_results) / len(timing_results),
+        max_time=max(timing_results)
+    )
+
+
+def time_exercises(exercises: List[BenchmarkExercise],
+                   times: int = 10) -> BenchmarkResults:
+    len_exercise = len(exercises)
+    exercise_results = []
+    for index, exercise in enumerate(exercises, start=1):
+        print(f"Start exercise {index}/{len_exercise}")
+        exercise_results.append(time_exercise(exercise, times))
+        print(f"Finished exercise {index}/{len_exercise}")
+    return BenchmarkResults(
+        results=exercise_results
+    )
