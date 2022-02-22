077c36a5e028d5840b5eb66ff8f1611eef25cb07
Niko Strijbol <strijbol.niko@gmail.com>
2022-02-22 13:50:51 +0100

    Improve command line interface
    - The option for the config was still called testplan
    - Add -v option to enable logs

--- tested/__main__.py
+++ tested/__main__.py
@@ -6,23 +6,26 @@ from .utils import smart_close
 from .main import run
 
 parser = ArgumentParser(
-    description="The universal judge for Dodona."
+    description="The programming language agnostic educational test framework."
 )
-parser.add_argument('-p', '--testplan', type=FileType('r'),
-                    help="Where to read the configs from", default="-")
+parser.add_argument('-c', '--config', type=FileType('r'),
+                    help="Where to read the config from", default="-")
 parser.add_argument('-o', '--output', type=FileType('w'),
                     help="Where the judge output should be written to.",
                     default="-")
+parser.add_argument('-v', '--verbose', dest='verbose',
+                    help='Include verbose logs. It is recommended to also use -o in this case.',
+                    action='store_true')
 parser = parser.parse_args()
 
-# Disable logging
-# log = logging.getLogger()
-# log.setLevel(logging.DEBUG)
-# ch = logging.StreamHandler(stream=sys.stdout)
-# formatter = logging.Formatter('%(name)s:%(levelname)s:%(message)s')
-# ch.setFormatter(formatter)
-# log.addHandler(ch)
+if parser.verbose:
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(stream=sys.stdout)
+    formatter = logging.Formatter('%(name)s:%(levelname)s:%(message)s')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
 
-configuration = read_config(parser.testplan)
+configuration = read_config(parser.config)
 with smart_close(parser.output) as out:
     run(configuration, out)
