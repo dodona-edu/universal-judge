c5f4248a3750c364b080079057ca8ac296eb42a7
Niko Strijbol <strijbol.niko@gmail.com>
2023-04-17 13:38:15 +0200

    Move pytest config into one file

--- tests/conftest.py
+++
@@ -1,4 +0,0 @@
-def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark tests as slow")
-    config.addinivalue_line("markers", "linter: run linter tests")
-    config.addinivalue_line("markers", "haskell: mark as an Haskell exercise")
