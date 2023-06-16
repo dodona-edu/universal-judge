17d66c9daa55b8ee9a1a3361568a39fdae82a030
Niko Strijbol <strijbol.niko@gmail.com>
2023-06-16 13:32:36 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -6,10 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     devshell = {
       url = "github:numtide/devshell";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
