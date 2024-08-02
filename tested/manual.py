9a8dde2878085d2f995bc1d8b413222da829b485
jorg-vr <jorg.vr@live.be>
2024-08-02 10:59:45 +0200

    Solve isbn list

--- tested/manual.py
+++ tested/manual.py
@@ -13,7 +13,7 @@ from tested.configs import DodonaConfig, Options
 from tested.main import run
 from tested.testsuite import SupportedLanguage
 
-exercise_dir = "/home/jorg/Documents/universal-judge/tests/exercises/isbn"
+exercise_dir = "/home/jorg/Documents/universal-judge/tests/exercises/isbn-list"
 
 
 def read_config() -> DodonaConfig:
