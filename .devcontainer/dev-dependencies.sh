13a297bb698247752e39c3d31b0147d39fa4e261
jorg.vr <jorg.vanrenterghem@ugent.be>
2024-10-30 16:58:06 +0100

    Get rid of poetry

--- .devcontainer/dev-dependencies.sh
+++ .devcontainer/dev-dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Install dev dependencies
-
-# install poetry
-pip install poetry --user
-# Install dependencies
-python -m poetry install --with dev
+pip install --no-cache-dir --upgrade \
+    pytest==8.2.1 \
+    pytest-mock==3.14.0 \
+    pytest-cov==5.0.0 \
+    pytest-xdist==3.6.1 \
+    syrupy==4.6.1 \
+    black==24.4.2 \
+    isort==5.13.2 \
+    pyright==1.1.387
