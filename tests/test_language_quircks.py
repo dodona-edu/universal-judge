e51e4903d082185e583333691ae32da02597dc9b
breblanc <Brent.Blanckaert@UGent.be>
2024-12-11 11:31:05 +0100

    Adding extra test

--- tests/test_language_quircks.py
+++ tests/test_language_quircks.py
@@ -51,6 +51,12 @@ def test_typescript_set_typing(tmp_path: Path, pytestconfig: pytest.Config):
 
     assert result in valid_results
 
+def test_typescript_function_call_typing(tmp_path: Path, pytestconfig: pytest.Config):
+    statement_string = "test = {'test', True, testing(10)}"
+    result = convert_statement(parse_string(statement_string), full=True)
+
+    assert result == 'let test : Set<any> = new Set(["test", true, testing(10)])'
+
 
 def test_javascript_vanilla_object(tmp_path: Path, pytestconfig: pytest.Config):
     conf = configuration(
