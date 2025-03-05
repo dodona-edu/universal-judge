934157518ec4a473edf4a66f00a69cf562beeaf6
jorg.vr <jorg.vanrenterghem@ugent.be>
2025-03-05 10:37:46 +0100

    Fix test_specific_oracle_exception_syntax_error

---
+++ tests/exercises/division/evaluation/evaluator_syntax_error.cpp
@@ -0,0 +1,11 @@
+#include <exception>
+#include "evaluation_result.h"
+
+class Evaluator {asd
+public: asfs
+    EvaluationResult* evaluate(const std::exception_ptr &actual) {
+       sad throw std::out_of_range("hello");
+    }
+};
+
+Evaluator* evaluator_syntax_error = new Evaluator();
